a222ed9364e5c4768f8c5e566a10f77dab69bf2c
James Grieve <pocojames@hotmail.com>
2021-02-24 23:19:05 -0800

    add user stuff and session sign-in

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+        session[:user_id] = @user.id
+        flash.notice = "Account succesfully created!"
+        redirect_to root_path
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
+end
